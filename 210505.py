364f2983f5ae541c40ef90f368ecb90b202dd83f
tony25842 <tony25842@gmail.com>
2021-05-10 21:00:52 +0800

    自訂函數練習

--- 210505.py
+++ 210505.py
@@ -1,8 +1,31 @@
-def greting(name):
+def greting(Fname):
     """這是一個函數的說明"""
-    print(" %s 晚上好" % (name))
+    print("%s 晚上好" % (Fname))
+
+def gretingv2(Fname = "First",Lname = "Last"):
+    """這是一個有兩個參數函數的說明"""
+    print("%s %s 晚上好" % (Fname,Lname))
+
+def gretingv3(Fname ,Lname):
+    result = Fname + " " + Lname + " 晚上好"
+    return result
+
+def gretingv4(lists):
+    for l in lists:
+        print(l + " 晚上好")
 
 
-name = "Tony"
+Fname = "Tony"
+Lname = "Lu"
 help(greting)
-greting(name)
+greting(Fname)
+gretingv2(Fname,Lname)
+
+gretingv2(Fname = "Lu",Lname = "Tony") #具名呼叫
+gretingv2(Lname = "Tony" ,Fname = "Lu")
+gretingv2()#預設值
+gretingv2("Tony")
+
+print(gretingv3(Fname, Lname))
+
+gretingv4(["Tony Lu","Jackie Chen","Eric Yu"])
